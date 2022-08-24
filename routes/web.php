beb2a812a6f8cc3b4fb6a3ba1bdfa9ffbc2fe2a0
fracabu <100199965+fracabu@users.noreply.github.com>
2022-08-24 18:47:42 +0200

    mod

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,15 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 //home con autenticazione
 Route::get('/admin', 'Admin\HomeController@index')->name('admin.index');
+Route::middleware("auth")
+    ->namespace("Admin") // indica la cartella dove si trovano i controller
+    ->name("admin.") // Aggiungie prima del nome di ogni rotta questo prefisso
+    ->prefix("admin") // Aggiunge prima di ogni URI questo pregisso
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+        Route::get('/test', 'HomeController@test')->name('test');
+        // Route::get('/posts/create', 'HomeController@test')->name('posts.create');
+        // Route::get('/posts/{post}/edit', 'HomeController@test')->name('posts.edit');
 
-Route::resource ("posts", "PostController");
+        Route::resource("posts", "PostController");
+    });
